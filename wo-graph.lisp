0bd26d5237d2c67955553a8fb36a5ebc53183133
Willem Rein Oudshoorn <woudshoo+github@xs4all.nl>
2012-08-25 15:18:02 +0200

    Updated documentation.

--- wo-graph.lisp
+++ wo-graph.lisp
@@ -5,58 +5,58 @@
 ;;; Basic graph inquiry options
 
 (defgeneric all-vertices (graph)
-  (:documentation "Returns in a list all vertices of the `graph'."))
+  (:documentation "Returns in a list all vertices of the GRAPH."))
 
 (defgeneric outgoing-edges (vertex graph)
   (:documentation "A list of all edges which have as source vertex
-  `vertex' in the `graph'."))
+  VERTEX in the GRAPH."))
 
 (defgeneric incoming-edges (vertex graph)
   (:documentation "A list of all edges which have as target vertex
-  `vertex' in the `graph'."))
+  VERTEX in the GRAPH."))
 
 (defgeneric source-vertex (edge graph)
-  (:documentation "A list of source vertices for the `edge' in the
-`graph'.  Note that for all normal graphs this will return a list of
+  (:documentation "A list of source vertices for the EDGE in the
+GRAPH.  Note that for all normal graphs this will return a list of
 length one.  However for hyper graphs (?)  this can return more than
 one element."))
 
 (defgeneric target-vertex (edge graph)
-  (:documentation "A list of target vertices for the `edge' in the
-`graph'.  Note that for all normal graphs this will return a list of
+  (:documentation "A list of target vertices for the EDGE in the
+GRAPH.  Note that for all normal graphs this will return a list of
 length one.  However for hyper graphs (?)  this can return more than
 one element."))
 
 (defgeneric sources-of-vertex (vertex graph)
-  (:documentation "A list of all source vertices of `vertex' in
-`graph'.  A vertex `a' is a source of `vertex' if there is an edge
-which has `vertex' as a target s and `a' as a source.  The collection
+  (:documentation "A list of all source vertices of VERTEX in
+GRAPH.  A vertex `a' is a source of VERTEX if there is an edge
+which has VERTEX as a target s and `a' as a source.  The collection
 can contain duplicates if there are multiple edges connecting the
-source with `vertex'."))
+source with VERTEX."))
 
 (defgeneric targets-of-vertex (vertex graph)
-  (:documentation "A list of all target vertices of `vertex' in
-`graph'.  A vertex `b' is a target of `vertex' if there is an edge
-which has `vertex' as a source s and `b' as a target.  The collection
+  (:documentation "A list of all target vertices of VERTEX in
+GRAPH.  A vertex `b' is a target of VERTEX if there is an edge
+which has VERTEX as a source and `b' as a target.  The collection
 can contain duplicates if there are multiple edges connecting the
-`vertex' with a target."))
+VERTEX with a target."))
 
 (defgeneric neighbors-of-vertex (vertex graph)
-  (:documentation "A list of all neighbors of `vertex'.  
-A neighbor is defined as either a target or a source for the `vertex', 
-so this is equivalent of the union (with duplicates) of `sources-of-vertex' and
-`targets-of-vertex'."))
+  (:documentation "A list of all neighbors of VERTEX.  
+A neighbor is defined as either a target or a source for the VERTEX, 
+so this is equivalent of the union (with duplicates) of SOURCES-OF-VERTEX and
+TARGETS-OF-VERTEX."))
 
 (defgeneric get-vertex-marker (graph)
   (:documentation "Returns a vertex marker for the graph.  A vertex
   marker is a data structure that associates auxiliary data with
-  vertices of `graph'.  The twomethods that are available on a vertex
-  marker are `get-mark' and `(setf get-mark)'."))
+  vertices of GRAPH.  The twomethods that are available on a vertex
+  marker are GET-MARK and `(setf get-mark)'."))
 
 (defgeneric get-mark (object marker &optional default-value)
-  (:documentation "Gets the associated data of `object' (typically a
-  vertex of a graph) by the data structure `marker'.  If it is not
-  found it will return `default-value' (or nil if the default value is
+  (:documentation "Gets the associated data of OBJECT (typically a
+  vertex of a graph) by the data structure MARKER.  If it is not
+  found it will return DEFAULT-VALUE (or nil if the default value is
   not specified.
 
   The result is a place which can be set with setf."))
@@ -67,31 +67,44 @@ so this is equivalent of the union (with duplicates) of `sources-of-vertex' and
 ;; API For manipulating graph
 
 (defgeneric add-vertex (vertex graph)
-  (:documentation "Adds `vertex' to the `graph'.  If the same vertex
+  (:documentation "Adds VERTEX to the GRAPH.  If the same vertex
  is added multiple times it will only be added to the graph once.
  When two vertices are considered the same depends on the implementation
  of the graph."))
 (defgeneric add-edge (source target edge graph)
-  (:documentation "Adds an edge connecting from `source' vertex to the `target' vertex.
-If `edge' is nil a new edge will be created.  If `edge' is non nil the edge argument will
+  (:documentation "Adds an edge connecting from SOURCE vertex to the TARGET vertex.
+If EDGE is nil a default edge will be created.  If EDGE is not nil the edge argument will
 be added. If the same edge is added multiple times, only one edge is added.
 
-Note that by using different `edge' arguments you can add multiple edges between
-`source' and `target'."))
+It is important the realize a few important facts
+
+- All edges need to be different, and the behaviour of adding the same
+  edge twice but with different SOURCE and TARGET is undefined.
+
+- A nil edge creates a default edge depending on the SOURCE and
+  TARGET.  So it is safe to add nil edges multiple times and for
+  different source and targets.
+
+- Adding a nil edge twice for the same source and target will only add
+  the default edge once.
+
+- If multiple edges need to be added between the same source and
+  target, they need to have different edge arguments.
+"))
 
 (defgeneric remove-vertex (vertex graph)
-  (:documentation "Removes the `vertex' from the graph. 
-All edges incident to the `vertex' are removed as well."))
+  (:documentation "Removes the VERTEX from the graph. 
+All edges incident to the VERTEX are removed as well."))
 (defgeneric remove-edge (edge graph)
-  (:documentation "Removes the `edge' from the graph.  
+  (:documentation "Removes the EDGE from the graph.  
 No vertices are removed."))
 
 ;; API High level manipulating graph
 
 (defgeneric copy (graph)
-  (:documentation "Returns a copy of the `graph'.  This copy can share vertices and edges
-with the original graph, but all manipulations with `add-vertex', `add-edge'
-`remove-vertex' and `remove-edge' on the copy will not change the original graph.
+  (:documentation "Returns a copy of the GRAPH.  This copy can share vertices and edges
+with the original graph, but all manipulations with ADD-VERTEX, ADD-EDGE
+REMOVE-VERTEX and REMOVE-EDGE on the copy will not change the original graph.
  (nor the other way around). "))
 (defgeneric edge-test (graph))
 (defgeneric vertex-test (graph))
